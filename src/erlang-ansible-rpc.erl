bb9387d7ecf13736139d089b27ec2607fa2413bb
Rafal Studnicki <rafal.studnicki@erlang-solutions.com>
2016-03-16 09:58:42 +0100

    Make it ansible compliant and add an example playbook

--- src/erlang-ansible-rpc.erl
+++ src/erlang-ansible-rpc.erl
@@ -3,42 +3,43 @@
 %% API exports
 -export([main/1]).
 
+-record(opts, {node, function, args, cookie, module, nametype}).
+
 %%====================================================================
 %% API functions
 %%====================================================================
 
 %% escript Entry point
-main(_Args) ->
-    Result = do(),
+main(Args) ->
+    Result = do(Args),
     io:format("~s~n", [jsx:encode(Result)]),
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-do() ->
+do(Opts) ->
     try
-        start_network(),
-        Result = make_call(),
-        success(Result)
+        ModuleOpts = read_opts(Opts),
+        case check_opts(ModuleOpts) of
+            ok ->
+                start_network(ModuleOpts),
+                Result = make_call(ModuleOpts),
+                success(Result);
+            {missing, Missing} ->
+                failure([io_lib:format("Missing parameters: ~p", [Missing])])
+        end
     catch
-        throw:{no_env, Name} ->
-            failure(["Env variable ", Name, " does not exist"]);
-        throw:{badrpc, Reason} ->
-            failure(["Error during RPC: ", io_lib:format("~p", [Reason])]);
         Error:Reason ->
-            failure([io_lib:format("~p ~p", [Error, Reason])])
+            failure([io_lib:format("~p ~p ~p",
+                                   [Error, Reason, erlang:get_stacktrace()])])
     end.
 
-start_network() ->
-    {ok, _Pid} = net_kernel:start([atom(env("NAME")), atom(env("NAMETYPE"))]),
-    true = erlang:set_cookie(node(), atom(env("COOKIE"))).
+start_network(#opts{nametype=NameType, cookie=Cookie} = Opts) ->
+    {ok, _Pid} = net_kernel:start([node_name(Opts), NameType]),
+    true = erlang:set_cookie(node(), Cookie).
 
-make_call() ->
-    Node = atom(env("NODE")),
-    Module = atom(env("MODULE")),
-    Function = atom(env("FUNCTION")),
-    Args = parse_args(env("ARGS")),
+make_call(#opts{node=Node, module=Module, function=Function, args=Args}) ->
     case rpc:call(Node, Module, Function, Args) of
         {badrpc, Reason} ->
             throw({badrpc, Reason});
@@ -46,22 +47,55 @@ make_call() ->
             Result
     end.
 
-success(Result) ->
-    [{<<"result">>, iolist_to_binary(io_lib:format("~p", [Result]))}].
+read_opts([OptsFile]) ->
+    {ok, Content} = file:read_file(OptsFile),
+    {match, Values} = re:run(Content, <<"([^=]*)=\"([^\"]*)\"\s*">>,
+                             [global, {capture, [1,2], binary}]),
+    lists:foldl(fun value_to_opt/2, #opts{}, Values).
 
-failure(Msg) ->
-    [{<<"failed">>, <<"true">>}, {<<"msg">>, iolist_to_binary(Msg)}].
+check_opts(#opts{}=Opts) ->
+    [opts|Values] = tuple_to_list(Opts),
+    Fields = record_info(fields, opts),
+    case lists:filtermap(fun check_opt/1, lists:zip(Fields, Values)) of
+        [] -> ok;
+        Missing -> {missing, Missing}
+    end.
+
+check_opt({Name, undefined}) -> {true, Name};
+check_opt(_) -> false.
+
+value_to_opt([<<"node">>, Node], Opts) ->
+    Opts#opts{node = atom(Node)};
+value_to_opt([<<"function">>, Function], Opts) ->
+    Opts#opts{function = atom(Function)};
+value_to_opt([<<"args">>, Args], Opts) ->
+    Opts#opts{args = parse_args(fix_arguments_opt(Args))};
+value_to_opt([<<"cookie">>, Cookie], Opts) ->
+    Opts#opts{cookie = atom(Cookie)};
+value_to_opt([<<"module">>, Module], Opts) ->
+    Opts#opts{module = atom(Module)};
+value_to_opt([<<"nametype">>, NameType], Opts) ->
+    Opts#opts{nametype = atom(NameType)}.
+
+fix_arguments_opt(Args) ->
+    {match, [Match]} = re:run(Args, <<"'(.*)'">>, [{capture, [1], binary}]),
+    binary_to_list(Match).
 
 parse_args(String) ->
     {ok, Tokens, _} = erl_scan:string(String ++ "."),
     {ok, Args} = erl_parse:parse_term(Tokens),
     Args.
 
-atom(List) when is_list(List) ->
-    list_to_atom(List).
+success(Result) ->
+    [{<<"result">>, iolist_to_binary(io_lib:format("~p", [Result]))}].
 
-env(Name) ->
-    case os:getenv(Name) of
-        false -> throw({no_env, Name});
-        Value -> Value
-    end.
+failure(Msg) ->
+    [{<<"failed">>, <<"true">>}, {<<"msg">>, iolist_to_binary(Msg)}].
+
+node_name(#opts{node = Node}) ->
+    list_to_atom("rpc-caller-" ++ atom_to_list(Node)).
+
+atom(List) when is_list(List) ->
+    list_to_atom(List);
+atom(Binary) when is_binary(Binary) ->
+    binary_to_atom(Binary, utf8).
